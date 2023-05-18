b3651f778b7a1af2de87e0c6225cf7118a755a4f
Boris Mauricio Murillo Mejia <boris.mejia@wiedii.co>
2023-05-17 22:13:41 -0500

    agregada funcionalidad de guardado en local storage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,4 +11,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  userInput: string = '';
+  items: any[] = [];
+  totalItems: number = 0;
+
+  // escuhar el evento del boton enter
+  onEnterKey() {
+    this.userInput = this.userInput.trim();
+
+    if (this.userInput === '') {
+      return;
+    }
+
+    this.totalItems++;
+
+    this.items.push({
+      id: this.totalItems,
+      title: this.userInput,
+      completed: false
+    });
+
+    localStorage.setItem('mydayapp-angular', JSON.stringify(this.items));
+
+    this.userInput = '';
+  }
+
 }
